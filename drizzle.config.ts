2e23b85a200b993558ca0e0339f3f0e28bc1104b
PedroJGC <pedro.juliogc02@gmail.com>
2025-08-04 22:31:45 -0300

    chore: add drizzle setup with initial schema; add seed script

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,7 @@
 /** biome-ignore-all lint/style/noNonNullAssertion: DATABASE_URL is guaranteed to be defined in the environment */
 import 'dotenv/config'
 import { defineConfig } from 'drizzle-kit'
+
 export default defineConfig({
   out: './drizzle',
   schema: './src/db/schema.ts',
@@ -8,4 +9,6 @@ export default defineConfig({
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
+  verbose: true,
+  strict: true,
 })
