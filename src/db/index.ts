c491cff915d17f017b2ddc1690da237d09f3b768
PedroJGC <pedro.juliogc02@gmail.com>
2025-08-05 17:02:50 -0300

    chore: add better auth tables

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,8 @@
 import 'dotenv/config'
 import { drizzle } from 'drizzle-orm/node-postgres'
+import * as schema from './schema'
 
 // biome-ignore lint/style/noNonNullAssertion: DATABASE_URL is guaranteed to be defined in the environment
-export const db = drizzle(process.env.DATABASE_URL!)
+export const db = drizzle(process.env.DATABASE_URL!, {
+  schema,
+})
